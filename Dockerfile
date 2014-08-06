a2d4c68146bbfe37439451743c319c2fd9d77dd2
CMGS <ilskdw@gmail.com>
2014-08-06 19:51:11 +0800

    bug fix, add site tmpl

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM flynn/busybox
 MAINTAINER CMGS <ilskdw@gmail.com>
 
 ADD ./levi /bin/levi
+ADD ./etc/site.tmpl /etc/site.tmpl
 
 ENTRYPOINT ["/bin/levi"]
 CMD []
--- common.go
+++ common.go
@@ -4,7 +4,7 @@ const (
 	ADD_CONTAINER          = 1
 	REMOVE_CONTAINER       = 2
 	UPDATE_CONTAINER       = 3
-	NGINX_CONF_TMPL        = "etc/site.tmpl"
+	NGINX_CONF_TMPL        = "/etc/site.tmpl"
 	DEFAULT_HOME_PATH      = "/tmp"
 	CONTAINER_STOP_TIMEOUT = 15
 	NETWORK_MODE           = "bridge"
