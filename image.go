e13b265195a502ce21c9d20046bdead2488363e1
CMGS <ilskdw@gmail.com>
2014-08-11 14:10:33 +0800

    support run daemon, daemon docker will be named as appname_0

--- image.go
+++ image.go
@@ -32,42 +32,49 @@ func (self *Image) Pull() error {
 func (self *Image) Run(job *Task, uid int) (*docker.Container, error) {
 	image := fmt.Sprintf("%s/%s:%s", RegEndpoint, self.appname, self.version)
 
-	exposedPorts := make(map[docker.Port]struct{})
-	port := docker.Port(fmt.Sprintf("%d/tcp", job.Port))
-	exposedPorts[port] = struct{}{}
-
 	config := docker.Config{
-		CpuShares:    job.Cpus,
-		Memory:       job.Memory,
-		User:         strconv.Itoa(uid),
-		Image:        image,
-		Cmd:          job.Cmd,
-		Env:          []string{"RUNENV=PROD"},
-		WorkingDir:   fmt.Sprintf("/%s", self.appname),
-		ExposedPorts: exposedPorts,
+		CpuShares:  job.Cpus,
+		Memory:     job.Memory,
+		User:       strconv.Itoa(uid),
+		Image:      image,
+		Cmd:        job.Cmd,
+		Env:        []string{"RUNENV=PROD"},
+		WorkingDir: fmt.Sprintf("/%s", self.appname),
 	}
+
+	hostConfig := docker.HostConfig{
+		Binds: []string{
+			fmt.Sprintf("%s:%s:ro", self.configPath, fmt.Sprintf("/%s/config.yaml", self.appname)),
+			fmt.Sprintf("%s:%s", path.Join(Permdirs, self.appname), fmt.Sprintf("/%s/permdir", self.appname)),
+			"/var/run:/var/run",
+		},
+		NetworkMode: NetworkMode,
+	}
+
+	if !job.Daemon {
+		port := docker.Port(fmt.Sprintf("%d/tcp", job.Port))
+		exposedPorts := make(map[docker.Port]struct{})
+		exposedPorts[port] = struct{}{}
+		config.ExposedPorts = exposedPorts
+
+		portBindings := make(map[docker.Port][]docker.PortBinding)
+		portBindings[port] = []docker.PortBinding{{
+			HostIp:   "0.0.0.0",
+			HostPort: strconv.FormatInt(job.Bind, 10),
+		}}
+		hostConfig.PortBindings = portBindings
+	}
+
 	opts := docker.CreateContainerOptions{
 		fmt.Sprintf("%s_%d", self.appname, job.Bind),
 		&config,
 	}
+
 	container, err := self.client.CreateContainer(opts)
 	if err != nil {
 		return nil, err
 	}
-	portBindings := make(map[docker.Port][]docker.PortBinding)
-	portBindings[port] = []docker.PortBinding{{
-		HostIp:   "0.0.0.0",
-		HostPort: strconv.FormatInt(job.Bind, 10),
-	}}
-	permdir := path.Join(Permdirs, self.appname)
-	hostConfig := docker.HostConfig{
-		Binds: []string{
-			fmt.Sprintf("%s:%s:ro", self.configPath, fmt.Sprintf("/%s/config.yaml", self.appname)),
-			fmt.Sprintf("%s:%s", permdir, fmt.Sprintf("/%s/permdir", self.appname)),
-		},
-		PortBindings: portBindings,
-		NetworkMode:  NetworkMode,
-	}
+
 	if err := self.client.StartContainer(container.ID, &hostConfig); err != nil {
 		return nil, err
 	}
