ef7abc633adada663a773e9c2577a95655c479e5
CMGS <ilskdw@gmail.com>
2014-09-29 15:31:12 +0800

    add task test

---
+++ task_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_SetType(t *testing.T) {
+	task := Task{
+		Bind:   9999,
+		Daemon: "abc",
+		Test:   "def",
+	}
+	task.SetAsTest()
+	if !task.CheckTest() || !task.IsTest() {
+		t.Error("Test ident invaild")
+	}
+	task.SetAsDaemon()
+	if !task.CheckDaemon() || !task.IsDaemon() {
+		t.Error("Daemon ident invaild")
+	}
+	task.Daemon = ""
+	task.Test = ""
+	task.SetAsService()
+	if !task.ShouldExpose() || task.ident != "9999" {
+		t.Error("Service ident invaild")
+	}
+}
