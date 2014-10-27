364c0facdcf0891cc3fae9e715faca4e9934406c
CMGS <ilskdw@gmail.com>
2014-10-27 16:39:41 +0800

    add metrics test

--- metrics_test.go
+++ metrics_test.go
@@ -14,3 +14,15 @@ func Test_MetricData(t *testing.T) {
 		t.Error("Wrong apptype")
 	}
 }
+
+func Test_MetricReporter(t *testing.T) {
+	cid := "123"
+	Metrics.Add("test", cid, DEFAULT_TYPE)
+	if _, ok := Metrics.apps[cid]; !ok {
+		t.Error("Add Failed")
+	}
+	Metrics.Remove(cid)
+	if _, ok := Metrics.apps[cid]; ok {
+		t.Error("Remove Failed")
+	}
+}
