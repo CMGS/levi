d561de932d6b8ed81dbf6321162d9f447be59783
CMGS <ilskdw@gmail.com>
2014-11-10 15:58:26 +0800

    rollback, use old method to get interface stats

--- metrics/stats_darwin.go
+++ metrics/stats_darwin.go
@@ -34,3 +34,28 @@ func GetCgroupStats(id string) (m *cgroups.Stats, err error) {
 	}
 	return
 }
+
+func GetNetStats(cid string) (m map[string]uint64, err error) {
+	logs.Info("OSX not support get net stats")
+	rand.Seed(time.Now().UnixNano())
+	err = nil
+	m = map[string]uint64{}
+	m["inbytes.0"] = uint64(rand.Intn(10000))
+	m["inpackets.0"] = uint64(rand.Int63n(100000))
+	m["inerrs.0"] = 0
+	m["indrop.0"] = 0
+	m["outbytes.0"] = uint64(rand.Intn(10000))
+	m["outpackets.0"] = uint64(rand.Int63n(100000))
+	m["outerrs.0"] = 0
+	m["outdrop.0"] = 0
+	return
+}
+
+func NetNsSynchronize(pid string, fn func() error) (err error) {
+	logs.Info("OSX not support Setns.")
+	return fn()
+}
+
+func GetContainerPID(id string) (pid string, err error) {
+	return "1111111", nil
+}
