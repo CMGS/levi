2548729abbf39e3e73802ab952cd3568d7106df9
CMGS <ilskdw@gmail.com>
2014-12-09 13:54:43 +0800

    use exec command

--- metrics/stats_linux.go
+++ metrics/stats_linux.go
@@ -1,20 +1,10 @@
 package metrics
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"runtime"
-	"strconv"
-	"strings"
-
 	"../logs"
 
 	"github.com/docker/libcontainer/cgroups"
 	"github.com/docker/libcontainer/cgroups/fs"
-	"github.com/docker/libcontainer/system"
 )
 
 func InitDevDir() {
@@ -36,102 +26,4 @@ func GetCgroupStats(id string) (m *cgroups.Stats, err error) {
 		Name:   id,
 	}
 	return fs.GetStats(&c)
-}
-
-func GetNetStats(cid string) (map[string]uint64, error) {
-	var iStats map[string]uint64
-	_, _, pid, err := getLongID(cid)
-	if err != nil {
-		return nil, err
-	}
-	err = NetNsSynchronize(pid, func() (err error) {
-		var e error
-		iStats, e = getIfStats()
-		if e != nil {
-			return e
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	logs.Debug("Container net status", cid, iStats)
-	return iStats, nil
-}
-
-func NetNsSynchronize(pid string, fn func() error) (err error) {
-	runtime.LockOSThread()
-	defer runtime.UnlockOSThread()
-	f, err := os.OpenFile("/proc/self/ns/net", os.O_RDONLY, 0)
-	if err != nil {
-		return
-	}
-	defer func() {
-		system.Setns(f.Fd(), 0)
-		f.Close()
-	}()
-	if err = setNetNs(pid); err != nil {
-		return
-	}
-	return fn()
-}
-
-func setNetNs(pid string) (err error) {
-	path := filepath.Join("/proc", pid, "ns", "net")
-	f, err := os.OpenFile(path, os.O_RDONLY, 0)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-	if err = system.Setns(f.Fd(), 0); err != nil {
-		return
-	}
-	return
-}
-
-func getIfStats() (m map[string]uint64, err error) {
-	m = map[string]uint64{}
-	cmd := exec.Command("cat", "/proc/net/dev")
-	f, err := cmd.StdoutPipe()
-	if err != nil {
-		return
-	}
-	defer f.Close()
-	err = cmd.Start()
-	if err != nil {
-		return
-	}
-
-	s := bufio.NewScanner(f)
-	var d uint64
-	for i := 0; s.Scan(); {
-		var name string
-		var n [8]uint64
-		text := s.Text()
-		if strings.Index(text, ":") < 1 {
-			continue
-		}
-		ts := strings.Split(text, ":")
-		fmt.Sscanf(ts[0], "%s", &name)
-		if strings.HasPrefix(name, "veth") || name == "lo" {
-			continue
-		}
-		fmt.Sscanf(ts[1],
-			"%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
-			&n[0], &n[1], &n[2], &n[3], &d, &d, &d, &d,
-			&n[4], &n[5], &n[6], &n[7], &d, &d, &d, &d,
-		)
-		j := "." + strconv.Itoa(i)
-		m["inbytes"+j] = n[0]
-		m["inpackets"+j] = n[1]
-		m["inerrs"+j] = n[2]
-		m["indrop"+j] = n[3]
-		m["outbytes"+j] = n[4]
-		m["outpackets"+j] = n[5]
-		m["outerrs"+j] = n[6]
-		m["outdrop"+j] = n[7]
-		i++
-	}
-	err = cmd.Wait()
-	return
 }
